b7741f1d0b5503de8526ccbe3ed0f111deac8b68
ififi2017 <33808161+ififi2017@users.noreply.github.com>
2024-09-16 10:36:02 +0800

    fix: Fix the issue of incorrect metadata location.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import { ServiceWorkerRegistration } from "@/components/ServiceWorkerRegistration";
+import type { Metadata } from "next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -12,6 +13,22 @@ const geistMono = localFont({
   variable: "--font-geist-mono",
   weight: "100 900",
 });
+
+export const metadata: Metadata = {
+  title: "下班倒计时",
+  description: "倒计时显示距离下班还有多长时间",
+  applicationName: "下班倒计时",
+  appleWebApp: {
+    capable: true,
+    title: "下班倒计时",
+    statusBarStyle: "black-translucent",
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  themeColor: "#F3F4F6",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
+};
 
 export default function RootLayout({
   children,
